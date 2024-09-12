9035c2c865902f7d9f92a263632b52646278c120
Jesus Diaz <jesus.social@proton.me>
2024-09-12 16:10:14 +0800

    Update login.js

--- login.js
+++ login.js
@@ -1,14 +1,1 @@
-function togglePasswordVisibility() {
-    var passwordField = document.getElementById("password");
-    var toggleIcon = document.getElementById("toggleIcon");
-    
-    if (passwordField.type === "password") {
-        passwordField.type = "text";
-        toggleIcon.classList.remove("fa-eye");
-        toggleIcon.classList.add("fa-eye-slash");
-    } else {
-        passwordField.type = "password";
-        toggleIcon.classList.remove("fa-eye-slash");
-        toggleIcon.classList.add("fa-eye");
-    }
-}
+function togglePasswordVisibility(){var s=document.getElementById("password"),e=document.getElementById("toggleIcon");"password"===s.type?(s.type="text",e.classList.remove("fa-eye"),e.classList.add("fa-eye-slash")):(s.type="password",e.classList.remove("fa-eye-slash"),e.classList.add("fa-eye"))}
